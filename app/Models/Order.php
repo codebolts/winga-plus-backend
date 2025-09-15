e520b530d35db98e33db00a25ae39a95fc89b9a5
Baltazar Christian <121530115+T4z4r@users.noreply.github.com>
2025-09-15 23:09:54 +0300

    feat: added orders model and migrations

---
+++ app/Models/Order.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['buyer_id', 'total_price', 'status'];
+
+    public function buyer()
+    {
+        return $this->belongsTo(User::class, 'buyer_id');
+    }
+
+    public function items()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+}
