468cbfc26a99bf5639f57302b1473a0c45ed5a1c
Baltazar Christian <121530115+T4z4r@users.noreply.github.com>
2025-09-15 22:27:59 +0300

    feat: added product model and controller

---
+++ app/Models/Product.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'seller_id',
+        'category_id',
+        'subcategory_id',
+        'name',
+        'description',
+        'price',
+        'stock',
+        'image'
+    ];
+
+    public function seller()
+    {
+        return $this->belongsTo(User::class, 'seller_id');
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function subcategory()
+    {
+        return $this->belongsTo(Subcategory::class);
+    }
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
+}
